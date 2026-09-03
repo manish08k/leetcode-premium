c99f160607b4a28470512f06ba85dda2b87e1199
Manish <manishnalumachu@gmail.com>
2026-09-03 12:26:54 +0530

    Add solution: Brightest Position on Street

---
+++ LeetCode/brightest-position-on-street/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def brightestPosition(self, lights: List[List[int]]) -> int:
+        d = collections.defaultdict(int)
+        for i, dis in lights:
+            d[i-dis] += 1                            
+            d[i+dis+1] -= 1                          
+        cur, max_idx, max_val = 0, -1, -sys.maxsize
+        for idx, val in sorted(d.items()):            
+            cur += val
+            if cur > max_val:                         
+                max_val, max_idx = cur, idx
+        return max_idx
