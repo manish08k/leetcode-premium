768d289a062415b19ab1422b9ff187ed7b67d112
Manish <manishnalumachu@gmail.com>
2026-09-03 11:46:25 +0530

    Add solution: Flip Game II

---
+++ LeetCode/flip-game-ii/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canWin(self, s: str) -> bool:
+
+        def dfs(state):
+            arr = list(state)
+            n = len(arr)
+
+            for i in range(n-1):
+                if arr[i] == '+' and arr[i+1] == '+':
+                    arr[i],arr[i+1] = '-','-'
+                    next_state = ''.join(arr)
+
+                    if not dfs(next_state):
+                        arr[i],arr[i+1] = '+','+'
+                        return True
+
+                    arr[i],arr[i+1] = '+','+'
+
+            return False
+
+        return dfs(s)
