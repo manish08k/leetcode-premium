82ebd803abdd42dda9b2d74dda99b162c2913da3
Manish <manishnalumachu@gmail.com>
2026-09-03 11:58:09 +0530

    Add solution: Design Log Storage System

---
+++ LeetCode/find-the-derangement-of-an-array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDerangement(self, n: int) -> int:
+        if n == 0: return 1
+        if n == 1: return 0
+        dp = [0]*(n + 1)
+        dp[0] = 1
+        dp[1] = 0
+        for i in range(2, n + 1):
+            dp[i] = (i - 1)*(dp[i - 1] + dp[i - 2]) % 1000000007
+        return dp[n]
