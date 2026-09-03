c7cd8e91ed38baedb649f34354143806604480c8
Manish <manishnalumachu@gmail.com>
2026-09-03 11:44:28 +0530

    Add solution: Find the Celebrity

---
+++ LeetCode/find-the-celebrity/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findCelebrity(self, n: int) -> int:  
+
+        candidate = 0
+        for person in range(1,n):
+            res = knows(candidate, person)
+            if res:
+                candidate = person
+
+        for person in range(n):
+            if person == candidate:
+                continue
+            if knows(candidate, person) or not knows(person, candidate):
+                return -1
+
+        return candidate
