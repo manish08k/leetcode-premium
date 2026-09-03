6341cc5f0f1a096bdf24a7532ffaabcc53c01fec
Manish <manishnalumachu@gmail.com>
2026-09-03 09:50:01 +0530

    Add solution: Flip Game

---
+++ LeetCode/flip-game/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def generatePossibleNextMoves(self, currentState: str) -> List[str]:
+        result = []
+        for i in range(len(currentState) - 1):
+            if currentState[i:i+2] == "++":
+                next_state = currentState[:i] + "--" + currentState[i+2:]
+                result.append(next_state)
+        return result
