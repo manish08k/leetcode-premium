09ca7ff544d68d8949761c3a6a8b7a24de33b03e
Manish <manishnalumachu@gmail.com>
2026-09-03 12:05:12 +0530

    Add solution: Clone N-ary Tree

---
+++ LeetCode/clone-n-ary-tree/solution.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        node=Node(root.val)
+        for child in root.children:
+            node.children.append(self.cloneTree(child))
+        return node
