ffd055b698f2a56ab577ca534ce44a938d825357
Manish <manishnalumachu@gmail.com>
2026-09-03 13:03:30 +0530

    Add solution: Maximum Vacation Days

---
+++ LeetCode/maximum-vacation-days/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxVacationDays(self, flights: List[List[int]], days: List[List[int]]) -> int:
+        n, k = len(days), len(days[0]) 
+        max_vacation = [0] + [float('-inf') for _ in range(n-1)]
+
+        for week in range(k):
+            curr = [float('-inf') for _ in range(n)]
+
+            for dep_city in range(n):
+                for arr_city, flight_exists in enumerate(flights[dep_city]):
+                    if flight_exists or dep_city == arr_city:
+                        curr[arr_city] = max(curr[arr_city], max_vacation[dep_city] + days[arr_city][week])
+
+            max_vacation = curr
+
+        return max(max_vacation)
+        
