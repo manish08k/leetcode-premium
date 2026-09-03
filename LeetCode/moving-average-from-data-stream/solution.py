be87b181809323c503e85e0e8bade955e2d8f8d7
Manish <manishnalumachu@gmail.com>
2026-09-03 09:53:32 +0530

    Add solution: Moving Average from Data Stream

---
+++ LeetCode/moving-average-from-data-stream/solution.py
@@ -0,0 +1,22 @@
+from collections import deque
+class MovingAverage:
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.length = 0
+        self.max = size
+        self.sum = 0
+        self.queue = deque()
+
+    def next(self, val: int) -> float:
+        if self.length < self.max:
+            self.length +=1
+            self.queue.append(val)
+            self.sum += val
+        else:
+            self.sum -= self.queue.popleft()
+            self.queue.append(val)
+            self.sum += val
+        return self.sum/self.length
+        
