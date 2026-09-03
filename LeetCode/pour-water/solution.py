ded8ab9070846f011a87329fc8d2c6b3bd0614aa
Manish <manishnalumachu@gmail.com>
2026-09-03 12:00:21 +0530

    Add solution: Pour Water

---
+++ LeetCode/pour-water/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pourWater(self, heights: List[int], volume: int, k: int) -> List[int]:
+        for _ in range(volume):
+            min_left = min_right = heights[k]
+            idx = k
+
+            for i in range(k, -1, -1):
+                if heights[i] < min_left:
+                    idx = i
+                    min_left = heights[i]
+                elif heights[i] > min_left:
+                    break
+            
+            if idx != k:
+                heights[idx] += 1
+                continue 
+
+            for i in range(k, len(heights)):
+                if heights[i] < min_right:
+                    idx = i
+                    min_right = heights[i]
+                elif heights[i] > min_right:
+                    break
+
+            heights[idx] += 1
+        return heights
